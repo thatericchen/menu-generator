7bdc862a0a96c6829c5ccdc0182ddd954c6b98a0
Eric Chen <echen@lawn-143-215-97-63.lawn.gatech.edu>
2023-11-01 16:24:30 -0400

    routing

--- menu-gen-frontend/src/App.tsx
+++ menu-gen-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing";
+import InputPage from "./pages/input";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/input" element={<InputPage />} />
       </Routes>
     </BrowserRouter>
   );
