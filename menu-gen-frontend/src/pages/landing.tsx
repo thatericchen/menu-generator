3e8c0d3b0956cf0dc3e7cc3608f4a116381e186c
Eric Chen <echen@lawn-143-215-106-118.lawn.gatech.edu>
2023-11-02 22:23:57 -0400

    more ui changes

--- menu-gen-frontend/src/pages/landing.tsx
+++ menu-gen-frontend/src/pages/landing.tsx
@@ -4,15 +4,13 @@ import { useNavigate } from "react-router-dom";
 const LandingPage = () => {
   const navigate = useNavigate();
 
-  const commonFontFamily = '"Montserrat", sans-serif';
-
   const titleStyle = {
     fontSize: "4rem",
     color: "#0070f3",
     fontWeight: "bold",
     textShadow: "2px 2px 8px rgba(0, 0, 0, 0.2)",
     marginBottom: "0.5rem",
-    fontFamily: commonFontFamily,
+    fontFamily: '"Montserrat", sans-serif',
   };
 
   const sloganStyle = {
@@ -20,19 +18,7 @@ const LandingPage = () => {
     color: "#333",
     fontWeight: "normal",
     marginBottom: "2rem",
-    fontFamily: commonFontFamily,
-  };
-
-  const buttonStyle = {
-    backgroundColor: "#0070f3",
-    color: "white",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    border: "none",
-    fontSize: "1rem",
-    cursor: "pointer",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-    fontFamily: commonFontFamily,
+    fontFamily: '"Montserrat", sans-serif',
   };
 
   return (
@@ -48,9 +34,21 @@ const LandingPage = () => {
     >
       <h1 style={titleStyle}>QuickMenu</h1>
       <h2 style={sloganStyle}>Your digital menu, a scan away!</h2>
-      <Button onClick={() => navigate("/input")} style={buttonStyle}>
+      <Button
+        onClick={() => navigate("/input")}
+        style={{
+          backgroundColor: "#0070f3",
+          color: "white",
+          padding: "10px 20px",
+          borderRadius: "5px",
+          border: "none",
+          fontSize: "1rem",
+          cursor: "pointer",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+        }}
+      >
         Enter
-      </Button>
+      </Button>{" "}
     </div>
   );
 };
