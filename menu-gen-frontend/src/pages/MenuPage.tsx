adfbc7dcb6920217f5f4a5754b00e1b83f5152f7
jamesli12 <leej55064@gmail.com>
2023-11-28 01:18:22 -0500

    Fixing frontend

--- menu-gen-frontend/src/pages/MenuPage.tsx
+++ menu-gen-frontend/src/pages/MenuPage.tsx
@@ -2,25 +2,16 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Card, CardHeader, CardBody, Image } from '@nextui-org/react';
 import { Menu } from './MenusPage';
-import { useNavigate } from 'react-router-dom';
-
 
 const MenuPage = () => {
   const [menu, setMenu] = useState<Menu | null>(null);
   const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate();
-
+  
   useEffect(() => {
     if (id) {
       fetchMenu(id);
     }
   }, [id]);
-
-  const backtomenu = () => {
-    navigate('/menus');
-  };
-
-
 
   const fetchMenu = async (id: string) => {
     try {
@@ -47,47 +38,40 @@ const MenuPage = () => {
   }
 
   return (
-      <div className="min-h-screen p-5" style={{ backgroundColor: 'black' }}>
-        <button onClick={backtomenu} className="mb-4 px-4 py-2 bg-gray-300 text-black rounded">
-            Back to Menus
-        </button>
-        <Card className="max-w-[400px] mx-auto pb-9" style={{ backgroundColor: 'wheat' }}>
-          <CardHeader className="flex flex-col gap-3 items-start">
-            <h1 className="text-4xl font-bold mb-4" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{menu.restaurant_name}</h1>
-            <p className="mb-2" style={{ color: 'black' , fontFamily: 'Apple Chancery, cursive' }}>{menu.restaurant_slogan}</p>
-            {menu.restaurant_logo_url && (
-                <Image
-                    src={menu.restaurant_logo_url}
-                    alt={menu.restaurant_name}
-                    width={256}
-                    height={256}
-                    className="mb-4"
-                />
-            )}
-          </CardHeader>
-          <CardBody className="flex flex-col gap-3 items-center mb-2">
-            {menu.food_items.map((item) => (
-                <Card key={item.title} className="mb-4 flex flex-col items-center min-w-[256px] pb-4" style={{ backgroundColor: 'wheat' }}>
-                  <h3 className="text-xl font-bold mb-1" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{item.title}</h3>
-                  <p className="mb-1" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{item.description}</p>
-                  <p className="font-bold" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{'$' + (item.price ? item.price : 0)}</p>
-                  <p className="mb-1" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{item.dietary_restrictions}</p>
-                  <p className="mb-1" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{item.vegetarian ? 'Vegetarian' : ''}</p>
-                  <p className="mb-1" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{item.spicy ? 'Spicy' : ''}</p>
-                  <p className="mb-1" style={{ color: 'black', fontFamily: 'Apple Chancery, cursive' }}>{item.gluten_free ? 'Gluten-Free' : ''}</p>
-                  {item.picture_url && (
-                      <Image
-                          src={item.picture_url}
-                          alt={item.title}
-                          width={128}
-                          height={128}
-                      />
-                  )}
-                </Card>
-            ))}
-          </CardBody>
-        </Card>
-      </div>
+    <div className="min-h-screen p-5 bg-gray-100">
+      <Card shadow-sm className="max-w-[400px] pb-9">
+        <CardHeader className="flex flex-col gap-3">
+          <h1 className="text-4xl font-bold mb-4">{menu.restaurant_name}</h1>
+          <p className="mb-2">{menu.restaurant_slogan}</p>
+          <Image
+            src={menu.restaurant_logo_url}
+            alt={menu.restaurant_name}
+            width={256}
+            height={256}
+            className="mb-4"
+          />
+        </CardHeader>
+        <CardBody className="flex flex-col gap-3 items-center mb-2">
+          {menu.food_items.map((item) => (
+            <Card key={item.title} className="mb-4 flex flex-col items-center min-w-[256px] pb-4">
+              <h3 className="text-xl font-bold mb-1">{item.title}</h3>
+              <p className="mb-1">{item.description}</p>
+              <p>{'$' + (item.price ? item.price : 0)}</p>
+              <p className="mb-1">{item.dietary_restrictions}</p>
+              <p className="mb-1">{item.vegetarian ? 'Vegetarian' : ''}</p>
+              <p className="mb-1">{item.spicy ? 'Spicy' : ''}</p>
+              <p className="mb-1">{item.gluten_free ? 'Gluten-Free' : ''}</p>
+              <Image
+                src={item.picture_url}
+                alt={item.title}
+                width={128}
+                height={128}
+              />
+            </Card>
+          ))}
+        </CardBody>
+      </Card>
+    </div>
   );
 };
 
